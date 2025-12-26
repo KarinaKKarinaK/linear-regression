b69817c1ab55362afa1e49cc44d9e76adfc279d9
Karina Barbara Kalicka-Molin <karina@Karinas-MacBook-Pro.local>
2025-12-26 22:12:21 +0200

    Adding basic functions to linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -11,5 +11,19 @@ plt.scatter(data.YearsExperience, data.Salary, color='blue')
 # plt.ylabel('Salary')
 # plt.show()
 
-def loss_function(): # same as "mean square error"
-    pass
+def loss_function(m, b, points): # same as "mean square error", 
+    # tell us how much we're off from the actual value
+    # we don't actually use it in the final optimization process,
+    # what we're actually intersted in is minimizng it
+    total_error = 0
+    for i in range(len(points)):
+        x = points.iloc[i].YearsExperience
+        y = points.iloc[i].Salary
+        total_error += (y - (m * x + b)) ** 2
+    total_error / float(len(points))
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
