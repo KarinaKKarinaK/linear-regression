e4826f562684961170a31e5b9ed919b67df41978
Karina Barbara Kalicka-Molin <karina@Karinas-MacBook-Pro.local>
2025-12-26 22:18:37 +0200

    Completed linear reghression

--- linear_regression.py
+++ linear_regression.py
@@ -27,3 +27,30 @@ def gradient_descent(m_now, b_now, points, L):
     b_gradient = 0
 
     n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].YearsExperience
+        y = points.iloc[i].Salary
+
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+
+    return m, b
+
+m = 0
+b = 0
+L = 0.0001 # learning rate
+epochs = 1000
+
+for i in range(epochs):
+    if i % 50 == 0:
+        print(f"Epoch {i}: m = {m}, b = {b}")   
+    m, b = gradient_descent(m, b, data, L)
+
+print(m, b)
+plt.scatter(data.YearsExperience, data.Salary, color='black')
+plt.plot(list(range(0, 20)), [m * x + b for x in range(0, 20)], color='red')
+plt.show()
